82bfb5511e49f66e77d4d14262893f390825bf3e
Philipp Scomparin <philipp.scomparin@yahoo.it>
2020-12-19 19:24:10 +0100

    added machine learning algorithms

--- src/main/scala/upm/bd/group4/App.scala
+++ src/main/scala/upm/bd/group4/App.scala
@@ -1,16 +1,15 @@
 package upm.bd.group4
 
-import org.apache.spark.ml.evaluation.RegressionEvaluator
+
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.{DataFrame, SparkSession, functions}
 import org.apache.spark.sql.functions._
 import org.apache.spark.ml.feature.{Normalizer, OneHotEncoder, StringIndexer, VectorAssembler, VectorIndexer}
-import org.apache.spark.ml.regression.{GBTRegressor, LinearRegression, RandomForestRegressionModel, RandomForestRegressor}
-import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
-import org.apache.spark.ml.{Pipeline, PipelineModel}
-import org.apache.spark.mllib.evaluation.RegressionMetrics
-import org.apache.spark.sql.types.{DataType, IntegerType, StringType}
-import org.apache.spark.ml.regression.DecisionTreeRegressionModel
-import org.apache.spark.ml.regression.DecisionTreeRegressor
+import org.apache.spark.ml.regression._
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.regression.{RandomForestRegressionModel, RandomForestRegressor}
+
 
 
 
@@ -20,15 +19,17 @@ import org.apache.spark.ml.regression.DecisionTreeRegressor
 object App {
 
   def main(args : Array[String]) {
+    val conf = new SparkConf().setAppName("Spark Practical Work")
     val spark = SparkSession.builder().getOrCreate()
-    spark.sparkContext.setLogLevel("FATAL")
+    
 
+    spark.sparkContext.setLogLevel("FATAL")
     import spark.implicits._
 
-  /*  print("\n")
-    print("Big Data Spark project group4\n")
-    print("Marivicitr Hang\nPhilipp Scomparin\nSili Liu\n")
-*/
+     print("\n")
+      print("Big Data Spark project group4\n")
+      print("Marivicitr Hang\nPhilipp Scomparin\nSili Liu\n")
+ 
 
     val wholeData = spark.read.format("csv").load("*.csv")
       .withColumnRenamed("_c0", "Year")
@@ -72,8 +73,7 @@ object App {
       .drop("NASDelay")
       .drop("SecurityDelay")
       .drop("LateAircraftDelay")
-      .withColumn("ArrDelay",col("ArrDelay").cast("integer"))
-      .withColumn("CRSElapsedTime",col("CRSElapsedTime").cast("integer"))
+      
 
     println("\nAllowedData: " + allowedData.count() + " rows")
 
@@ -96,23 +96,40 @@ object App {
       .drop("CancellationCode")
       .drop("Cancelled")
       .drop("DepTime")
-      .na.drop()
+      .drop("TailNum")
+      .na.drop("any")
 
     println("FilteredData: "+filteredDF.count() + " rows")
     filteredDF.show(5)
- //   filteredDF.printSchema()
+    //   filteredDF.printSchema()
 
-   /*
-    println(filteredDF.select("Cancelled").collect().map(_(0)).toList)
+    /*
+     println(filteredDF.select("Cancelled").collect().map(_(0)).toList)
 
-    filteredDF.agg(max(filteredDF(filteredDF.columns(10))), min(filteredDF(filteredDF.columns(10)))).show
-    filteredDF.agg(max(filteredDF(filteredDF.columns(9))), min(filteredDF(filteredDF.columns(9)))).show
-  */
+     filteredDF.agg(max(filteredDF(filteredDF.columns(10))), min(filteredDF(filteredDF.columns(10)))).show
+     filteredDF.agg(max(filteredDF(filteredDF.columns(9))), min(filteredDF(filteredDF.columns(9)))).show
+   */
+
+/*
+    val indexer = new StringIndexer()
+      .setInputCols(Array("UniqueCarrier","Origin","Dest"))
+      .setOutputCols(Array("UniqueCarrierIndex","OriginIndex","DestIndex"))
+    val indexed = indexer.fit(filteredDF).transform(filteredDF)
+
+    val oneHotEncoder = new OneHotEncoder()
+      .setInputCols(Array("UniqueCarrierIndex","OriginIndex","DestIndex"))
+      .setOutputCols(Array("UniqueCarrierVec","OriginVec","DestVec"))
+      .setDropLast(false)
+    val oneHotEncoded = oneHotEncoder.fit(indexed).transform(indexed)
+
+    */
 
 
-    val categFeatures = Array("UniqueCarrier","TailNum","Origin","Dest")
+    val categFeatures = Array("UniqueCarrier","Origin","Dest")
     println("Categorical features: " + categFeatures.mkString("Array(", ", ", ")"))
- //   categColumns.show(5)
+    // categColumns.show(5)
+
+
 
 
     val encodedFeatures = categFeatures.flatMap{name =>
@@ -126,120 +143,224 @@ object App {
         .setOutputCols(Array(name + "Vec"))
         .setDropLast(false)
 
-     Array(stringIndexer, oneHotEncoder)
-    //   Array(oneHotEncoder)
+      Array(stringIndexer, oneHotEncoder)
     }
+
 
     val pipeline = new Pipeline().setStages(encodedFeatures)
     val indexerModel = pipeline.fit(filteredDF)
     val dfTransformed = indexerModel.transform(filteredDF)
 
-  //  dfTransformed.show(5)
-  //  dfTransformed.printSchema()
 
 
     val assembler = new VectorAssembler()
-      .setInputCols(Array("UniqueCarrierVec","TailNumVec","OriginVec","DestVec","Year","Month","DayOfMonth","DayOfWeek","CRSDepTime","CRSArrTime","CRSElapsedTime","FlightNum","DepDelay","Distance","TaxiOut"))
+      .setInputCols(Array("UniqueCarrierVec","OriginVec","DestVec","Year","Month","DayOfMonth","DayOfWeek","CRSDepTime","CRSArrTime","CRSElapsedTime","FlightNum","DepDelay","Distance","TaxiOut"))
       .setOutputCol("features")
-      
 
- //   assembler.show(3)
- //   assembler.printSchema()
+
+    val output = assembler.transform(dfTransformed)
+
 
     val normalizer = new Normalizer()
       .setInputCol("features")
       .setOutputCol("normFeatures")
       .setP(1.0)
-      
+    val NormData = normalizer.transform(output)
+    //NormData.select("features","normFeatures").show(false)
+
+    //  NormData.select("features","normFeatures").show(5)
+    //  NormData.printSchema()
+
+    val Array(trainingData, testData) = NormData.randomSplit(Array(0.7, 0.3))
 
 
-  //  normalizer.select("features","normFeatures").show(5)
-  //  normalizer.printSchema()
-
-    val Array(trainingData, testData) = dfTransformed.randomSplit(Array(0.7, 0.3))
-
-
-   //-------------------LINEAR REGRESSION---------------------------------------------
+    //-------------------LINEAR REGRESSION---------------------------------------------
 
     val lr = new LinearRegression()
       .setLabelCol("ArrDelay")
       .setFeaturesCol("normFeatures")
+      .setPredictionCol("predictionLR")
       .setMaxIter(10)
       .setRegParam(1.0)
       .setElasticNetParam(1.0)
 
+    // Fit the model
+    val lrModel = lr.fit(trainingData)
 
-    val pipeline2 = new Pipeline().setStages(Array(assembler, normalizer, lr))
-    val lrModel = pipeline2.fit(trainingData)
-    val lrResult = lrModel.transform(testData)
+    // print output and input column
+    val predictionsLR = lrModel.transform(testData)
+    //predictionsLR.select("normFeatures", "ArrDelay", "predictionLR").show()
 
-    lrResult.select("features", "normFeatures", "ArrDelay", "prediction").show()
+    val resultLR=predictionsLR.select("normFeatures","ArrDelay", "predictionLR")
+
+    println("-------------------------Linear Regression-------------------------")
+    resultLR.show()
+
+
+    // Print coeff and intercept
+    // println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+
+
+
+    // Summarize the model and print out some metrics
+    val trainingSummaryLR = lrModel.summary
+
+    trainingSummaryLR.residuals.show()
+    println(s"RMSE = ${trainingSummaryLR.rootMeanSquaredError}")
+    println(s"R-Squared = ${trainingSummaryLR.r2}")
+
+    //-------------------GAUSSIAN LINEAR REGRESSION---------------------------------------------
+
+    val glr = new GeneralizedLinearRegression()
+      .setFamily("gaussian")
+      .setLabelCol("ArrDelay")
+      .setFeaturesCol("normFeatures")
+      .setPredictionCol("predictionGLR")
+      .setMaxIter(10)
+      .setRegParam(1.0)
+
+    // Fit the model
+    val glrModel = glr.fit(trainingData)
+
+    // print output and input column
+    val predictionsGLR = glrModel.transform(testData)
+    println("-------------------------Gaussian Linear Regression-------------------------")
+    predictionsGLR.select("ArrDelay", "predictionGLR").show()
+    val resultGLR = predictionsGLR.select("normFeatures","ArrDelay", "predictionGLR")
+
+
+    // Summarize the model and print out some metrics
+    val trainingSummaryGLR = glrModel.summary
+
+    trainingSummaryGLR.residuals.show()
+
+    val evaluatorGLR_RMSE = new RegressionEvaluator()
+      .setLabelCol("ArrDelay")
+      .setPredictionCol("predictionGLR")
+      .setMetricName("rmse")
+      .evaluate(predictionsGLR)
+
+    println(s"Root Mean Squared Error = $evaluatorGLR_RMSE")
+
+    val evaluatorGLR_R2 = new RegressionEvaluator()
+      .setLabelCol("ArrDelay")
+      .setPredictionCol("predictionGLR")
+      .setMetricName("r2")
+      .evaluate(predictionsGLR)
+
+    println(s"R-Squared = $evaluatorGLR_R2")
+
+
+   //-------------------Decision tree regression---------------------------------------------
+
+    val dt = new DecisionTreeRegressor()
+      .setLabelCol("ArrDelay")
+      .setFeaturesCol("normFeatures")
+      .setPredictionCol("predictionDT")
+
+    val dtmodel = dt.fit(trainingData)
+    val predictionsDT = dtmodel.transform(testData)
+    println("-------------------------Decision Tree Regression-------------------------")
+
+    predictionsDT.select("ArrDelay", "predictionDT").show()
+
+    val resultDT = predictionsDT.select("normFeatures","ArrDelay", "predictionDT")
 
     // Select (prediction, true label) and compute test error.
-    val evaluator = new RegressionEvaluator()
+    val evaluatorDT_RMSE = new RegressionEvaluator()
       .setLabelCol("ArrDelay")
-      .setPredictionCol("prediction")
+      .setPredictionCol("predictionDT")
       .setMetricName("rmse")
-    val rmse = evaluator.evaluate(lrResult)
-      println(s"Root Mean Squared Error = $rmse")
+      .evaluate(predictionsDT)
 
-    val evaluatorR2 = new RegressionEvaluator()
-    .setLabelCol("ArrDelay")
-    .setPredictionCol("prediction")
-    .setMetricName("r2");
-    val r2 = evaluatorR2.evaluate(lrResult);
-    println(s"R2 = $r2")
+    println(s"Root Mean Squared Error = $evaluatorDT_RMSE")
 
-//     // Fit the model
-//     val lrModel = lr.fit(trainingData)
+    val evaluatorDT_R2 = new RegressionEvaluator()
+      .setLabelCol("ArrDelay")
+      .setPredictionCol("predictionDT")
+      .setMetricName("r2")
+      .evaluate(predictionsDT)
 
-//     // print output and input column
-//     lrModel.transform(testData).select("features","normFeatures", "ArrDelay", "prediction").show()
-
-//     // Print coeff and intercept
-//     println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
-
-//     // Summarize the model and print out some metrics
-//     val trainingSummary = lrModel.summary
-//  //   println(s"numIterations: ${trainingSummary.totalIterations}")
-//   //  println(s"objectiveHistory: [${trainingSummary.objectiveHistory.mkString(",")}]")
-//     trainingSummary.residuals.show()
-//     println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
-//     println(s"r2: ${trainingSummary.r2}")
+    println(s"R-Squared = $evaluatorDT_R2")
 
 
+    //-------------------RANDOM FOREST REGRESSION---------------------------------------------
 
-//------------------------------------------- Decision Tree ------------------------------------------------------------
-// Train a DecisionTree model.
-// val dt = new DecisionTreeRegressor()
-//   .setLabelCol("ArrDelay")
-//   .setFeaturesCol("normFeatures")
+        val rf = new RandomForestRegressor()
+          .setLabelCol("ArrDelay")
+          .setFeaturesCol("normFeatures")
+          .setPredictionCol("predictionRF")
 
-// // Chain indexer and tree in a Pipeline.
-// val pipeline2 = new Pipeline()
-//   .setStages(Array(dt))
+        val modelRF = rf.fit(trainingData)
 
-// // Train model. This also runs the indexer.
-// val model = pipeline2.fit(trainingData)
+        // Make predictions.
+        val predictionsRF = modelRF.transform(testData)
 
-// // Make predictions.
-// val predictions = model.transform(testData)
+          println("-------------------------Random Forest Regression-------------------------")
 
-// // Select example rows to display.
-// predictions.select("prediction", "ArrDelay", "features").show(5)
-
-// // Select (prediction, true label) and compute test error.
-// val evaluator = new RegressionEvaluator()
-//   .setLabelCol("ArrDelay")
-//   .setPredictionCol("prediction")
-//   .setMetricName("rmse")
-// val rmse = evaluator.evaluate(predictions)
-// println(s"Root Mean Squared Error (RMSE) on test data = $rmse")
-
-// val treeModel = model.stages(1).asInstanceOf[DecisionTreeRegressionModel]
-// println(s"Learned regression tree model:\n ${treeModel.toDebugString}")
+        // Select example rows to display.
+        predictionsRF.select("ArrDelay", "predictionRF").show()
 
 
+        val resultRF = predictionsRF.select("normFeatures", "ArrDelay", "predictionRF")
+
+
+        // Select (prediction, true label) and compute test error.
+        val evaluatorRF_RMSE = new RegressionEvaluator()
+          .setLabelCol("ArrDelay")
+          .setPredictionCol("predictionRF")
+          .setMetricName("rmse")
+          .evaluate(predictionsRF)
+
+        println(s"Root Mean Squared Error = $evaluatorRF_RMSE")
+
+        val evaluatorRF_R2 = new RegressionEvaluator()
+          .setLabelCol("ArrDelay")
+          .setPredictionCol("predictionRF")
+          .setMetricName("r2")
+          .evaluate(predictionsRF)
+
+        println(s"R-Squared = $evaluatorRF_R2")
+
+
+    //------------------------------------------Summaries-----------------------------------------
+
+    val result_aux1 = resultGLR
+      .withColumnRenamed("normFeatures","norm")
+      .withColumnRenamed("ArrDelay","Arr")
+    val result_aux2 = resultLR.join(result_aux1,resultLR("normFeatures")===result_aux1("norm")&&resultLR("ArrDelay")===result_aux1("Arr"))
+    val result_aux3 = result_aux2
+      .drop("norm")
+      .drop("Arr")
+
+    val result_aux4 = resultRF
+      .withColumnRenamed("normFeatures","norm")
+      .withColumnRenamed("ArrDelay","Arr")
+    val result_aux5 = resultDT.join(result_aux4,resultDT("normFeatures")===result_aux4("norm")&&resultDT("ArrDelay")===result_aux4("Arr"))
+    val result_aux6 = result_aux5
+      .drop("normFeatures")
+      .drop("ArrDelay")
+
+    val result = result_aux3.join(result_aux6,result_aux3("normFeatures")===result_aux6("norm")&&result_aux3("ArrDelay")===result_aux6("Arr"))
+    result
+      .drop("norm")
+      .drop("Arr")
+      .show()
+
+    val summaryDF = Seq(
+      ("LINEAR REGRESSION",trainingSummaryLR.rootMeanSquaredError,trainingSummaryLR.r2),
+      ("GAUSSIAN LINEAR REGRESSION",evaluatorGLR_RMSE,evaluatorGLR_R2),
+      ("DECISION TREE REGRESSION",evaluatorDT_RMSE,evaluatorDT_R2),
+      ("RANDOM FOREST REGRESSION",evaluatorRF_RMSE,evaluatorRF_R2)
+    ).toDF("Algorithm","RMSE","R2")
+
+    summaryDF.show(false)
+
+
+/*
+
+
+*/
 
   }
 
